b0d86d85a084b532072197101ac1f61238dcdad5
Daniel Szwajkowski <zeazelu@gmail.com>
2021-11-29 00:47:07 +0100

    Historia zamówień

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,13 +1,17 @@
-import express from 'express';
+import express, { Router } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
-import {isAuth} from '../utils.js';
+import { isAuth } from '../utils.js';
 
 const orderRouter = express.Router();
+orderRouter.get('/mine', isAuth, expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+}));
 
-orderRouter.post('/', isAuth, expressAsyncHandler(async(req, res) =>{
-    if(req.body.orderItems.length === 0) {
-        res.status(400).send({message:'Koszyk jest pusty'});
+orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
+    if (req.body.orderItems.length === 0) {
+        res.status(400).send({ message: 'Koszyk jest pusty' });
     } else {
         const order = new Order({
             orderItems: req.body.orderItems,
@@ -19,15 +23,37 @@ orderRouter.post('/', isAuth, expressAsyncHandler(async(req, res) =>{
             user: req.user._id,
         });
         const createdOrder = await order.save();
-        res.status(201).send({message: 'Stworzono nowe zamówienie', order: createdOrder});
+        res.status(201).send({ message: 'Stworzono nowe zamówienie', order: createdOrder });
     }
 }));
-orderRouter.get('/:id', isAuth, expressAsyncHandler(async(req, res) =>{
+orderRouter.get('/:id', isAuth, expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
-    if(order) {
+    if (order) {
         res.send(order);
     } else {
-        res.status(404).send({message: 'Nie znaleziono zamówienia'});
+        res.status(404).send({ message: 'Nie znaleziono zamówienia' });
     }
 }));
+
+orderRouter.put(
+    '/:id/pay',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.paymentResult = {
+                id: req.body.id,
+                status: req.body.status,
+                update_time: req.body.update_time,
+                email_address: req.body.email_address,
+            };
+            const updatedOrder = await order.save();
+            res.send({ message: 'Zamówienie opłacone', order: updatedOrder });
+        } else {
+            res.status(404).send({ message: 'Nie znaleziono zamówienia' });
+        }
+    })
+);
 export default orderRouter;
