ef85ab1d23924f30dac86a7bf64b0e2f11edf531
Daniel Szwajkowski <zeazelu@gmail.com>
2021-11-29 21:32:51 +0100

    Panel admina

--- backend/utils.js
+++ backend/utils.js
@@ -28,4 +28,11 @@ export const isAuth = (req, res, next) =>{
     } else {
         res.status(401).send({message: 'Brak tokenu'});
     }
+};
+export const isAdmin = (req, res, next) =>{
+    if(req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({message: 'Niepoprawny token admina'});
+    }
 }
